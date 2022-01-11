84cf4ee5cdf357aabb07b73b6291800efc636aaa
dk89 <david.kiraly1@gmail.com>
2022-01-11 22:25:15 +0100

    add SimpleLineChart chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PieChartWithCustomizedLabel from './charts/PieChartWithCustomizedLabel';
+import SimpleLineChart from './charts/SimpleLineChart';
 
 const Chart = (props: any) => {
     switch(props.chartType) {
         case "PieChartWithCustomizedLabel":
             return <PieChartWithCustomizedLabel { ...props } />;
+        case "SimpleLineChart":
+            return <SimpleLineChart { ...props } />;
         default:
             return null;
     }
---
+++ src/components/charts/SimpleLineChart.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
+const SimpleLineChart = (props: any) => {
+
+    const dayData = props.data;
+
+    const data = [
+        { name: 'infected', value: dayData.infected },
+        { name: 'deceased', value: dayData.deceased },
+        { name: 'recovered', value: dayData.recovered },
+        { name: 'quarantined', value: dayData.quarantined },
+        { name: 'tested', value: dayData.tested }
+    ];
+
+    return (
+        <div>
+            <ResponsiveContainer width="100%" height={300}>
+                <LineChart
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                    >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="value" stroke="#82ca9d" />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    );
+}
+
+export default SimpleLineChart;
